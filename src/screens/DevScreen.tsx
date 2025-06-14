e567ce1533d05e47d9326ea323c11687f2b445d8
Claude Wild <38697367+imisshtml@users.noreply.github.com>
2025-06-13 23:38:44 -0400

    Updated DevScreen.tsx

--- src/screens/DevScreen.tsx
+++ src/screens/DevScreen.tsx
@@ -279,7 +279,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     padding: 20,
-    //paddingTop: 40,
     borderBottomWidth: 1,
     borderBottomColor: '#333',
   },
